c5fb3cec7aed3105efdb007f493faad6f7ab3cd9
ceisman21 <ceisman21@kentdenver.org>
2021-12-13 18:55:00 -0700

    Constructors

--- src/JsonReader.java
+++ src/JsonReader.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 public class JsonReader {
+    private Scanner reader;
+    private File file;
+    private ArrayList attributes;
+    private ArrayList values;
+
+    public JsonReader() {
+        file = null;
+        reader = null;
+        attributes = new ArrayList();
+        values = new ArrayList();
+    }
+
+    public JsonReader(File file) {
+        this.file = file;
+
+        try {
+            reader = new Scanner(file);
+        } catch (FileNotFoundException exception) {
+            reader = null;
+        }
+
+        attributes = new ArrayList();
+        values = new ArrayList();
+    }
+
 }
